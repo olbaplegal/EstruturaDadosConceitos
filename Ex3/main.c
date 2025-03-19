61587ae1cd6b636ae5b198d31ee114d921821be2
olbaplegal <olbaplegal@gmail.com>
2025-03-18 22:52:30 -0300

    adicionado exercicio 3 de arrays

---
+++ Ex3/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <limits.h>
+int main() {
+int vetor[20]; int tamanho = 0; int menor = INT_MAX;
+printf("Digite o tamanho do vetor (max 20): ");
+scanf("%d", &tamanho);
+if (tamanho > 20) 
+{
+    printf("Tamanho excede o limite do vetor.\n");
+    return 1; // Encerra o programa com erro
+}
+for (int i = 0; i < tamanho; i++) 
+{
+    printf("Digite o valor para o indice %d: ", i);
+    scanf("%d", &vetor[i]); 
+}
+for (int i = 0; i < tamanho; i++)
+{
+    if (vetor[i] < menor)
+    {
+        menor = vetor[i];
+    }
+}
+printf("O menor numero no vetor e: %d", menor);
+return 0;
+}
