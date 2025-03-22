e35f02ea89a470c0bf9c857555dae8c790457b07
olbaplegal <olbaplegal@gmail.com>
2025-03-22 20:43:27 -0300

    atualização dos arquivos para rodar na ultima versão do comilador GCC

--- Ex2/main.c
+++ Ex2/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main() {
-int vetor[20]; int tamanho = 0; int soma = 0; int media = 0;
+int vetor[20]; int tamanho = 0; int soma = 0; int media = 0;int i = 0;
 printf("Digite o tamanho do vetor (max 20): ");
 scanf("%d", &tamanho);
 if (tamanho > 20) 
@@ -8,16 +8,16 @@ if (tamanho > 20)
     printf("Tamanho excede o limite do vetor.\n");
     return 1; // Encerra o programa com erro
 }
-for (int i = 0; i < tamanho; i++) 
+for (i = 0; i < tamanho; i++) 
 {
     printf("Digite o valor para o indice %d: ", i);
     scanf("%d", &vetor[i]); 
 }
-for (int i = 0; i < tamanho; i++)
+for (i = 0; i < tamanho; i++)
 {
     soma = soma + vetor[i];
 }
-for (int i = 0; i < tamanho; i++)
+for (i = 0; i < tamanho; i++)
 {
     printf("\nvetor %d = %d", i, vetor[i]);
 }
--- Ex3/main.c
+++ Ex3/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <limits.h>
 int main() {
-int vetor[20]; int tamanho = 0; int menor = INT_MAX;
+int vetor[20]; int tamanho = 0; int menor = INT_MAX;int i = 0;
 printf("Digite o tamanho do vetor (max 20): ");
 scanf("%d", &tamanho);
 if (tamanho > 20) 
@@ -9,12 +9,12 @@ if (tamanho > 20)
     printf("Tamanho excede o limite do vetor.\n");
     return 1; // Encerra o programa com erro
 }
-for (int i = 0; i < tamanho; i++) 
+for (i = 0; i < tamanho; i++) 
 {
     printf("Digite o valor para o indice %d: ", i);
     scanf("%d", &vetor[i]); 
 }
-for (int i = 0; i < tamanho; i++)
+for (i = 0; i < tamanho; i++)
 {
     if (vetor[i] < menor)
     {
